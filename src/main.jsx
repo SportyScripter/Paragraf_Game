58d16b1299a9d4b21a66a621e5979a313be0e956
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2023-12-26 22:42:03 +0100

    add first background image

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,8 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home';
-
-import './index.css';
+import Home from './Home'; // Upewnij się, że ścieżka jest poprawna
+import './App.css'
 
 ReactDOM.render(
   <React.StrictMode>
